73d7eee4c79f3967619572f11338996c5006556f
HungNhOnPV <anhhungx9propro@gmail.com>
2019-11-22 22:18:40 +0700

    Show Message Sketeton when Messages Loading

--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -8,6 +8,7 @@ import MessagesHeader from './MessagesHeader';
 import MessageForm from './MessageForm';
 import Message from './Message';
 import Typing from './Typing';
+import Skeleton from './Skeleton';
 
 class Messages extends React.Component {
     state = {
@@ -236,10 +237,20 @@ class Messages extends React.Component {
         ))
     )
 
+    displayMessageSkeleton = loading => 
+        loading ? (
+            <React.Fragment>
+                {[...Array(10)].map((_, i) => (
+                    <Skeleton key={i} />
+                ))}
+            </React.Fragment>
+        ) : null;
+
     render() {
         // prettion-ignore
         const { messagesRef, messages, channel, user, progressBar, numUniqueUsers, searchTerm, 
-        searchResults, searchLoading, privateChannel, isChannelStarred, typingUsers } = this.state;
+        searchResults, searchLoading, privateChannel, isChannelStarred, typingUsers, 
+        messagesLoading } = this.state;
         
         return (
             <React.Fragment>
@@ -255,6 +266,7 @@ class Messages extends React.Component {
 
                 <Segment>
                     <Comment.Group className={progressBar ? 'messages__progress' : 'messages'}>
+                        {this.displayMessageSkeleton(messagesLoading)}
                         {searchTerm 
                             ? this.displayMessages(searchResults) 
                             : this.displayMessages(messages)}
